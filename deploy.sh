8312fc8dc2e325c6bf5ae35cea426d8b41b65368
Sebastian Curtis T. Lavarias <sebastiancurtislavarias@gmail.com>
2026-01-10 16:42:21 +0800

    revision(docker compose): new service added.

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ echo "===== Pulling latest code ====="
 git pull origin main
 
 echo "===== Rebuilding and restarting containers ====="
-docker compose -f docker-compose.prod.yml up -d --build kislap_flash kislap_admin kislap_web_server
+docker compose -f docker-compose.prod.yml up -d --build kislap_flash kislap_admin kislap_web_server kislap_marketing
 
 echo "===== Cleaning up old images ====="
 docker image prune -f
